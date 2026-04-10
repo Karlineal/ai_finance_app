5658300c231f186bb170cb15d1f22439ecd27fbf
彭家城   20231060285 <pjc13403080718@gmail.com>
2026-04-10 11:58:35 +0800

    feat(database): 注册 ScheduledRule 到数据库模块
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- core/database/src/main/java/com/aifinance/core/database/AiFinanceDatabase.kt
+++ core/database/src/main/java/com/aifinance/core/database/AiFinanceDatabase.kt
@@ -5,9 +5,11 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.aifinance.core.database.dao.AccountDao
 import com.aifinance.core.database.dao.CategoryDao
+import com.aifinance.core.database.dao.ScheduledRuleDao
 import com.aifinance.core.database.dao.TransactionDao
 import com.aifinance.core.database.entity.AccountEntity
 import com.aifinance.core.database.entity.CategoryEntity
+import com.aifinance.core.database.entity.ScheduledRuleEntity
 import com.aifinance.core.database.entity.TransactionEntity
 
 @Database(
@@ -15,8 +17,9 @@ import com.aifinance.core.database.entity.TransactionEntity
         AccountEntity::class,
         CategoryEntity::class,
         TransactionEntity::class,
+        ScheduledRuleEntity::class,
     ],
-    version = 4,
+    version = 5,
     exportSchema = true,
 )
 @TypeConverters(Converters::class)
@@ -24,4 +27,5 @@ abstract class AiFinanceDatabase : RoomDatabase() {
     abstract fun accountDao(): AccountDao
     abstract fun categoryDao(): CategoryDao
     abstract fun transactionDao(): TransactionDao
+    abstract fun scheduledRuleDao(): ScheduledRuleDao
 }
